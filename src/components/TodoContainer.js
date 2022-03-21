514d2f5437f9c63e3f1214c5766ad754f2fd8b04
Cynthia Inga <candyinga67@gmail.com>
2022-03-21 15:59:48 +0200

    Add style to the App

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -60,7 +60,8 @@ class TodoContainer extends React.Component {
   };
   render() {
     return (
-      <div>
+      <div className="container">
+        <div className="inner">
         <Header />
         <InputTodo addTodoProps={this.addTodoItem} />
         <TodosList
@@ -68,6 +69,7 @@ class TodoContainer extends React.Component {
           handleChangeProps={this.handleChange}
           deleteTodoProps={this.delTodo}
         />
+        </div>
       </div>
     );
   }
