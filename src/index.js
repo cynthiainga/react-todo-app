5cff52605865de18544fc5141fa7721bba4e6686
Cynthia Inga <candyinga67@gmail.com>
2022-03-21 11:27:46 +0200

    Add the to do container

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TodoContainer from './components/TodoContainer';
 
-const element = <h1>Hello from Creat React Application</h1>;
-
-ReactDOM.render(element, document.getElementById("root"))
+ReactDOM.render(<TodoContainer />, document.getElementById("root"))
